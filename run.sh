551379b4291934f9abb5d64796495a5f629507ca
Hossein <hgh.comphys@gmail.com>
2018-11-28 16:22:10 +0100

    files added via upload

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo 'It may take a while...'
+for i in tip5p # spce tip3p tip4p tip5p reax
+do
+	echo $i
+	mpirun -np 4 lmp_mpi < $i.in > $i.out
+	
+	cd out
+	mv ../rdf.dat rdf-$i.dat
+	mv ../dump.xyz dump-$i.xyz
+	mv ../$i.out .
+	cd ../	
+done
+rm -f log.lammps log.cite
+echo 'done.'
+echo 
